50f8be65da3d8aa30c3014a772ce1b6121d1a251
Salma890592 <shaiksalma5051@gmail.com>
2021-03-02 11:30:25 +0530

    Initial commit

---
+++ NamedParameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamedParameters
+{
+    class Program
+    {
+        static void GetCohortDetails(string Cohort, int Genc, string Mode, string Track, string Current)
+        {
+            Console.WriteLine($"It is {Cohort} with {Genc} GenCs undergoing training for {Track} thru {Mode}.The current module of training is {Current}");
+        }
+        static void Main(string[] args)
+        {
+            GetCohortDetails(Cohort: "ADM20DF003", Mode: "OBL", Genc: 21, Track: ".Net", Current: "C#Advance");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Nunit2/Calculator1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace NUnit2
+{
+    interface IMathLibrary
+    {
+        double Addition(double a, double b);
+        double Subtraction(double a, double b);
+        double Multiplication(double a, double b);
+        double Division(double a, double b);
+    }
+
+    public class Calculator1 : IMathLibrary
+    {
+        double result = 0;
+        public double Addition(double a, double b)
+        {
+            result = a + b;
+            return result;
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            result = a - b;
+            return result;
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            result = a * b;
+            return result;
+        }
+
+        public double Division(double a, double b)
+        {
+            
+            result = a / b;
+            return result;
+        }
+
+        public void AllClear()
+        {
+            result = 0;
+        }
+
+        public double GetResult
+        {
+            get { return result; }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using NUnit.Framework;
+//using NUnit2; 
+
+
+//namespace NUnit2
+//{
+
+//    [TestFixture]
+//    public class CalculatorTests
+//    {
+//        private Calculator calculator;
+
+
+//        [SetUp]
+//        public void SetUp()
+//        {
+//            calculator = new Calculator();
+
+//        }
+
+//        [Test]
+//        [TestCase(2, 1, 3)]
+//        //[TestCase(2,1,4)]
+//        public void addMethod(int a, int b, int sum)
+//        {
+
+//            var result = calculator.Add(a, b);
+
+//            Assert.That(result, Is.EqualTo(sum));
+//        }
+//        static void Main(string[] args)
+//      {
+//      }
+
+
+//        //[TearDown]
+//        //public void CleanUpMethod()
+//        //{
+//        //    sum = 0;
+//        //}
+
+//    }
+//}
