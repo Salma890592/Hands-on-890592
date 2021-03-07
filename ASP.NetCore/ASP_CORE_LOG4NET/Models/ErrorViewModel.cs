946f75d8c7d759f0173ed0a6e1266c66d24c5c81
Salma890592 <shaiksalma5051@gmail.com>
2021-03-07 21:52:50 +0530

    hands on
    hands on

---
+++ ASP.NetCore/ASP_CORE_LOG4NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP_CORE_LOG4NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
